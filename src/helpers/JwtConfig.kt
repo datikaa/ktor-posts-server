5cda1dbe728b8c82bf029e1e219df518e5508786
daniel.dattler <daniel.dattler@bcsconsult.hu>
2020-07-24 00:16:18 +0200

    Create JwtConfig

---
+++ src/helpers/JwtConfig.kt
@@ -0,0 +1,34 @@
+package com.datikaa.helpers
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
+import com.datikaa.model.UserRegistration
+import org.koin.core.KoinComponent
+import java.util.*
+
+object JwtConfig : KoinComponent {
+
+    private const val secret = "secretOfLifeIs42"
+    private const val issuer = "com.datikaa"
+    private const val validityInMs = 60_000 // 10 mins
+    private val algorithm = Algorithm.HMAC512(secret)
+
+    val verifier: JWTVerifier = JWT
+        .require(algorithm)
+        .withIssuer(issuer)
+        .build()
+
+    fun generateToken(userRegistration: UserRegistration): String {
+        return JWT.create()
+            .withSubject("Authentication")
+            .withIssuer(issuer)
+            .withClaim("name", userRegistration.email)
+            .withClaim("pw", userRegistration.passwordHash)
+            .withExpiresAt(getExpiration())
+            .sign(algorithm)
+    }
+
+    private fun getExpiration() = Date(System.currentTimeMillis() + validityInMs)
+}
+
